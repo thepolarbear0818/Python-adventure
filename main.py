a771fb33a35244f51df6fbee3da62898d4dffc0d
overfl0wyt <62394004+thepolarbear0818@users.noreply.github.com>
2022-01-12 20:38:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#123
+#this is quite odd 
+print("hello my friend")
+a = input()
+if a == "goodbye":
+  print("lameeeeeee")
+
+if a == "You're mum":
+  print("You are my mum")
+  
+  b = input()
+
+  if b == ("I don't have a child"):
+    print("You created me therefore you are my parent")
+
+
+
+
+else:
+  print("beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep")
+
