a7cf0b071a0ecbb8aeb9e904af2fa39650ea055d
Henry Dong <62394004+thepolarbear0818@users.noreply.github.com>
2022-01-12 20:45:58 +0000

    hhahadashdhashdahs funny mums

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ a = input()
 if a == "goodbye":
   print("lameeeeeee")
 
-if a == "You're mum":
+if a == "You're mum" or "Your mom" or "You're mom" or "you're mum ":
   print("You are my mum")
   
   b = input()
@@ -13,9 +13,6 @@ if a == "You're mum":
   if b == ("I don't have a child"):
     print("You created me therefore you are my parent")
 
-
-
-
 else:
-  print("beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep")
+  print("beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep malfunction detected")
 
