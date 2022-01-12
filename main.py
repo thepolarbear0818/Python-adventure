8368108f8cb576fe445aeb569462906cac0a1e65
Henry Dong <62394004+thepolarbear0818@users.noreply.github.com>
2022-01-12 21:00:45 +0000

    funny mum detecter

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 #123
 #this is quite odd 
+import os
+import requests
+import json 
+import random 
+
 print("hello my friend")
-a = input()
-if a == "goodbye":
+
+e = input()
+
+mums = ["your mum","you're mum","mum","mom"]
+if input() == "goodbye":
   print("lameeeeeee")
 
-if a == "You're mum" or "Your mom" or "You're mom" or "you're mum ":
-  print("You are my mum")
-  
-  b = input()
 
-  if b == ("I don't have a child"):
+
+if any(word in e for word in mums): # dont change this or that will break it, it looks for a word match in mums and e is our input
+  print("You are my mum : hit enter")
+  
+  
+
+  if e == ("I don't have a child"):
     print("You created me therefore you are my parent")
+    
+#if any(word in e for word in mums):
+  #print("this mum thing works i guess")
+
+if any(word in e for word in moms):
+  print("you are my mom ")  
 
 else:
   print("beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep malfunction detected")
